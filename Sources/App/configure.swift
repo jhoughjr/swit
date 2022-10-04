5b96f793ac7ff4765b97599aa76d3478e2a000db
Jimmy Hough <jimmyhoughjr@me.com>
2022-10-04 10:27:11 -0500

    added TLS

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -6,5 +6,11 @@ public func configure(_ app: Application) throws {
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
     // register routes
+    // Enable TLS.
+     let f = try! NIOSSLCertificate.fromPEMFile("/etc/letsencrypt/live/jimmyhoughjr.freeddns.org/fullchain.pem")
+    app.http.server.configuration.tlsConfiguration = .makeServerConfiguration(
+        certificateChain: f.map { .certificate($0) },
+        privateKey: .file("/etc/letsencrypt/live/jimmyhoughjr.freeddns.org/privkey.pem")
+    )
     try routes(app)
 }
