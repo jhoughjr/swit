10181c860b700dc9d97359a5253d5cf62e392140
Jimmy Hough <jimmyhoughjr@me.com>
2022-10-05 07:26:46 -0500

    new command runner

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,18 +8,36 @@ func routes(_ app: Application) throws {
 
     app.post { req async -> Response in
         req.logger.info("\(req.body)")
-        
-        let command = runAsync("swift", "build")
-            .onCompletion { command in
-            // be notified when the command is finished.
-            app.logger.info(" completed \(command.stdout)")
-        }
-        command.stdout.onOutput { stdout in
-            // be notified when the command produces output (only on macOS).
-            app.logger.info("got \(stdout.read())")
-        }
+        let foo =  runCommand(cmd: "swift", args: ["build"])
+        app.logger.info("\(foo)")
+//
+//        let command = runAsync("swift", "build")
+//            .onCompletion { command in
+//            // be notified when the command is finished.
+//            app.logger.info(" completed \(command.stdout)")
+//        }
+//        command.stdout.onOutput { stdout in
+//            // be notified when the command produces output (only on macOS).
+//            app.logger.info("got \(stdout.read())")
+//        }
 
         return .init(status:.ok)
     }
     
+     func runCommand(cmd: String,
+                                   args: [String]) -> String {
+        let outPipe = Pipe()
+        let proc = Process()
+        proc.launchPath = cmd
+        let foo = args.map{$0.replacingOccurrences(of: "\r", with: "\n")}
+        proc.arguments = foo
+        proc.standardOutput = outPipe
+        proc.launch()
+        proc.waitUntilExit()
+
+        let data = outPipe.fileHandleForReading.readDataToEndOfFile()
+        let res =  String(data: data, encoding: .utf8) ?? "ERROR"
+        //should trim?
+        return res
+    }
 }
