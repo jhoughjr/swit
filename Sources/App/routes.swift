a12e260d971c9c1d18f011ebc38d8389f671ec85
Jimmy Hough <jimmyhoughjr@me.com>
2022-10-05 07:02:06 -0500

    wip

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -6,11 +6,12 @@ func routes(_ app: Application) throws {
         "SWift gIT 1.0"
     }
 
-    app.post { req async in
+    app.post { req async -> Response in
         req.logger.info("\(req.body)")
         
         let command = runAsync("swift", "build").onCompletion { command in
             // be notified when the command is finished.
+            req.logger.info("\(command.stdout)")
         }
         command.stdout.onOutput { stdout in
             // be notified when the command produces output (only on macOS).
@@ -18,10 +19,13 @@ func routes(_ app: Application) throws {
         }
 
         // do something with ‘command’ while it is still running.
-
-        let foo = try? command.finish() // wait for it to finish.
-        req.logger.info("\(foo!.stdout.read())")
-        return 200
+        do {
+            let foo = try command.finish() // wait for it to finish.
+            req.logger.info("\(foo.stdout.read())")
+        }catch {
+            return .init(status:.internalServerError)
+        }
+        return .init(status:.ok)
     }
     
 }
